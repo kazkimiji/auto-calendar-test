9a8ea211a5a1112cb15c0953b8ae573f4081f802
Kaz Kimijima <kazuyakimijima55@gmail.com>
2022-10-07 11:36:41 -0400

    Commited to push to github

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AutoCalendar from "@/views/AutoCalendar";
+import AddContact from "@/views/AddContact";
+import EditCalendar from "@/views/EditCalendar";
+import ViewContact from "@/views/ViewContact";
+import PageNotFound from "@/views/PageNotFound";
 
 const routes = [
   {
     path: '/',
     name: 'home',
+    redirect: "/autocalendar",
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/autocalendar',
+    name: 'AutoCalendar',
+    component: AutoCalendar
+  },
+  {
+    path: '/contacts/add',
+    name: 'AddContact',
+    component: AddContact
+  },
+  {
+    path: '/calendars/edit/:contactId',
+    name: 'EditCalendar',
+    component: EditCalendar
+  },
+  {
+    path: '/contacts/view/:contactId',
+    name: 'ViewContact',
+    component: ViewContact
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'PageNotFound',
+    component: PageNotFound
   }
 ]
 
